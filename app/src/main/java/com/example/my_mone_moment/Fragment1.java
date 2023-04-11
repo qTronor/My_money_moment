524dfa290cf2542f127f830ce23793eff5ccd50f
Roman Kuzmenko <79635884+qTronor@users.noreply.github.com>
2023-04-11 12:41:01 +0300

    Added animation for FloatingBtn + added second btn for fragment2

--- app/src/main/java/com/example/my_mone_moment/Fragment1.java
+++ app/src/main/java/com/example/my_mone_moment/Fragment1.java
@@ -15,6 +15,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class Fragment1 extends Fragment {
+
+    boolean isRotateFloatBtn = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,15 +24,11 @@ public class Fragment1 extends Fragment {
 
 
         FloatingActionButton fab = view.findViewById(R.id.floatingActionButton1);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(view1 -> {
+            ViewAnimation.rotateFab(view1, !isRotateFloatBtn);
+            Snackbar.make(view1, "Fragment1", Snackbar.LENGTH_LONG)
+                   .setAction("Action", null).show();
 
-            @Override
-            public void onClick(View view) {
-                System.out.println("CLICK");
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-            }
         });
         return view;
     }
--- app/src/main/java/com/example/my_mone_moment/Fragment2.java
+++ app/src/main/java/com/example/my_mone_moment/Fragment2.java
@@ -9,10 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class Fragment2 extends Fragment {
+    boolean isRotateFloatBtn = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return (ViewGroup)inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+
+        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton1);
+        fab.setOnClickListener(view1 -> {
+            ViewAnimation.rotateFab(view1, !isRotateFloatBtn);
+            Snackbar.make(view1, "Fragment2", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        });
+        return view;
     }
 }
